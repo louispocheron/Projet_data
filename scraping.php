3fbbb672ee2d145e00d9987ff71aca8aecffe399
louispocheron <pocheron.louisamazon1@gmail.com>
2021-07-26 09:59:22 +0200

    contenu total scrapé

--- scraping.php
+++ scraping.php
@@ -155,3 +155,182 @@ echo $html->find('p', 5)->plaintext;
 echo $html->find('a[class="image"]', 0);
 ?>
 
+
+
+
+<!-- GO  -->
+
+
+</br>   
+    </br>
+
+<?php
+$html = file_get_html("https://fr.wikipedia.org/wiki/Go_(langage)");
+preg_match_all('/<img[^>]+>/i',$html, $result); 
+
+echo $html->find('h1[class="firstHeading"]', 0)->plaintext;
+?>
+</br>
+<?php
+echo $html->find('p', 1)->plaintext;
+echo $html->find('p', 2)->plaintext;
+echo $html->find('p', 3)->plaintext;
+echo $html->find('p', 4)->plaintext;
+
+
+
+?>
+</br>
+
+<?php
+
+echo $html->find('a[class="image"]', 0);
+?>
+
+
+
+
+
+<!-- KOTLIN -->
+
+</br>   
+    </br>
+
+<?php
+$html = file_get_html("https://fr.wikipedia.org/wiki/Kotlin_(langage)");
+preg_match_all('/<img[^>]+>/i',$html, $result); 
+
+echo $html->find('h1[class="firstHeading"]', 0)->plaintext;
+?>
+</br>
+<?php
+echo $html->find('p', 1)->plaintext;
+echo $html->find('p', 2)->plaintext;
+echo $html->find('p', 3)->plaintext;
+echo $html->find('p', 4)->plaintext;
+
+
+
+?>
+</br>
+
+<?php
+
+echo $html->find('a[class="image"]', 1);
+?>
+
+
+
+<!-- swift -->
+
+</br>   
+    </br>
+
+<?php
+$html = file_get_html("https://fr.wikipedia.org/wiki/Swift_(langage_d%27Apple)");
+preg_match_all('/<img[^>]+>/i',$html, $result); 
+
+echo $html->find('h1[class="firstHeading"]', 0)->plaintext;
+?>
+</br>
+<?php
+echo $html->find('p', 1)->plaintext;
+echo $html->find('p', 2)->plaintext;
+echo $html->find('p', 3)->plaintext;
+echo $html->find('p', 4)->plaintext;
+
+
+
+?>
+</br>
+
+<?php
+
+echo $html->find('a[class="image"]', 0);
+?>
+
+
+<!-- TYPESCRIPT -->
+
+</br>   
+    </br>
+
+<?php
+$html = file_get_html("https://fr.wikipedia.org/wiki/TypeScript");
+preg_match_all('/<img[^>]+>/i',$html, $result); 
+
+echo $html->find('h1[class="firstHeading"]', 0)->plaintext;
+?>
+</br>
+<?php
+echo $html->find('p', 1)->plaintext;
+echo $html->find('p', 2)->plaintext;
+echo $html->find('p', 3)->plaintext;
+// echo $html->find('p', 4)->plaintext;
+
+
+
+?>
+</br>
+
+<?php
+
+echo $html->find('a[class="image"]', 0);
+?>
+
+
+
+<!-- RUST -->
+
+</br>   
+    </br>
+
+<?php
+$html = file_get_html("https://fr.wikipedia.org/wiki/Rust_(langage)");
+preg_match_all('/<img[^>]+>/i',$html, $result); 
+
+echo $html->find('h1[class="firstHeading"]', 0)->plaintext;
+?>
+</br>
+<?php
+echo $html->find('p', 1)->plaintext;
+echo $html->find('p', 2)->plaintext;
+echo $html->find('p', 3)->plaintext;
+echo $html->find('p', 4)->plaintext;
+
+
+
+?>
+</br>
+
+<?php
+
+echo $html->find('a[class="image"]', 0);
+?>
+
+
+
+<!-- JULIA -->
+
+</br>   
+    </br>
+
+<?php
+$html = file_get_html("https://fr.wikipedia.org/wiki/Julia_(langage)");
+preg_match_all('/<img[^>]+>/i',$html, $result); 
+
+echo $html->find('h1[class="firstHeading"]', 0)->plaintext;
+?>
+</br>
+<?php
+echo $html->find('p', 1)->plaintext;
+echo $html->find('p', 2)->plaintext;
+echo $html->find('p', 3)->plaintext;
+echo $html->find('p', 4)->plaintext;
+?>
+</br>
+
+<?php
+
+echo $html->find('a[class="image"]', 1);
+?>
