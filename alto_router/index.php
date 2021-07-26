6a91024b4940a1f9fc5ba1f9261a5662949e6967
louispocheron <pocheron.louisamazon1@gmail.com>
2021-07-26 16:52:00 +0200

    l'alto_router

---
+++ alto_router/index.php
@@ -0,0 +1,19 @@
+<?php 
+require './vendor/autoload.php';
+$router = new AltoRouter();
+$router->setBasePath('Projet_data/');
+
+$router->map('GET', '/', 'test');
+$router->map('GET', '/langage/[*:langage]', 'langages/langage'); // langages = nom du dossier et langage = nom du fichier
+
+
+$match = $router->match();
+if(is_array($match)){
+    $params = $match ['params'];
+    require "{$match['target']}.php";
+}
+else{
+    echo '404';
+}
+
+
---
+++ alto_router/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3fbbb672ee2d145e00d9987ff71aca8aecffe399',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3fbbb672ee2d145e00d9987ff71aca8aecffe399',
+            'dev_requirement' => false,
+        ),
+        'altorouter/altorouter' => array(
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../altorouter/altorouter',
+            'aliases' => array(),
+            'reference' => '127f6e96998708a31ef32252985bea82e3b03888',
+            'dev_requirement' => false,
+        ),
+    ),
+);
