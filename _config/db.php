c005e7bbcac7284034fa617d1f60a5e1d9366c21
“laury <lquarto.lq@gmail.com>
2021-08-02 13:41:48 +0200

    changement nom database

--- _config/db.php
+++ _config/db.php
@@ -4,7 +4,7 @@
 try
 {
 // $bdd = new PDO('mysql:host=localhost;dbname=lauryq_db_projet_film;charset=utf8', 'lauryq', '5VfLpqpdqTzKgQ==');
- $bdd = new PDO('mysql:host=localhost;dbname=projet_film;charset=utf8', 'laury', 'quarto.l');
+ $bdd = new PDO('mysql:host=localhost;dbname=data_projet;charset=utf8', 'laury', 'quarto.l');
 }
 catch (Exception $e)
 {
